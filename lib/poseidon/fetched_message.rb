55e4aed941532afb021712c551f75bdc4fe530db
Andrew Kozin <andrew.kozin@gmail.com>
2015-08-29 20:31:53 +0300

    Add `#to_h` and `#to_hash` to `FetchedMessage`

--- lib/poseidon/fetched_message.rb
+++ lib/poseidon/fetched_message.rb
@@ -12,26 +12,42 @@ module Poseidon
   # end
   # ```
   #
-  # @param [String] topic
-  #   Topic this message should be sent to.
-  #
-  # @param [String] value
-  #   Value of the message we want to send. 
-  #
-  # @param [String] key
-  #   Optional. Message's key, used to route a message
-  #   to a specific broker.  Otherwise, keys will be
-  #   sent to brokers in a round-robin manner.
-  #
   # @api public
   class FetchedMessage
     attr_reader :value, :key, :topic, :offset
 
+    # Initializes the message
+    #
+    # @param [String] topic
+    #   Topic this message fetched from.
+    #
+    # @param [String] value
+    #   Value of the message.
+    #
+    # @param [String] key
+    #   Message's key.
+    #
+    # @param [Integer] offset
+    #   The offset of the message in a topic's partition.
+    #
+    # @api private
     def initialize(topic, value, key, offset)
       @topic  = topic
       @value  = value
       @key    = key
       @offset = offset
     end
+
+    # Converts the message to a hash
+    #
+    # @return [Hash]
+    #
+    # @alias :to_hash
+    #
+    # @api public
+    def to_h
+      { topic: topic, key: key, value: value, offset: offset }
+    end
+    alias_method :to_hash, :to_h
   end
 end
--- spec/unit/fetched_message_spec.rb
+++ spec/unit/fetched_message_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 RSpec.describe FetchedMessage do
+  let(:mts)  { FetchedMessage.new("hello_topic", "Hello World", "key", 0) }
+  let(:hash) do
+    { topic: "hello_topic", value: "Hello World", key: "key", offset: 0 }
+  end
+
   it "provides access to topic,value,key,offset" do
-    mts = FetchedMessage.new("hello_topic", "Hello World", "key", 0)
     expect(mts.topic).to eq("hello_topic")
     expect(mts.value).to eq("Hello World")
     expect(mts.key).to eq("key")
     expect(mts.offset).to eq(0)
+  end
+
+  describe "#to_h" do
+    subject { mts.to_h }
+
+    it "converts the message to hash" do
+      expect(subject).to eql(hash)
+    end
+  end
+
+  describe "#to_hash" do
+    subject { mts.to_hash }
+
+    it "converts the message to hash" do
+      expect(subject).to eql(hash)
+    end
   end
 end
